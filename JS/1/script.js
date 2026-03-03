3e5e16d9e621dc042b7e35802573b29114f086d5
Oleh Liubchenko <dev@liubquanti.click>
2026-03-03 13:53:20 +0100

    JS 1

---
+++ JS/1/script.js
@@ -0,0 +1,5 @@
+let minutes = window.prompt("Donner le nombre de minutes a convertir :");
+
+let seconds = minutes * 60;
+
+document.write(minutes + " minutes correspondent à " + seconds + " secondes");
---
+++ JS/2/script.js
@@ -0,0 +1,6 @@
+window.prompt("Donner la base d'un triangle :");
+window.prompt("Donner la hauteur d'un triangle :");
+
+let area = (base * height) / 2;
+
+document.write("L'aire du triangle est de " + area);
